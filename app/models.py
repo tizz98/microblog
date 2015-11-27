09cfd1df317347cf20347222bc1e7b4b8e20422b
tizz98 <dev.tizz98@gmail.com>
2015-11-27 14:36:05 -0500

    search

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
-from app import db
+import sys
+
+from app import db, app
 from hashlib import md5
+
+
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import flask.ext.whooshalchemy as whooshalchemy
 
 
 followers = db.Table(
@@ -80,6 +89,8 @@ class User(db.Model):
 
 
 class Post(db.Model):
+    __searchable__ = ['body']
+
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -88,3 +99,6 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post %r>' % self.body
 
+
+if enable_search:
+    whooshalchemy.whoosh_index(app, Post)
--- config.py
+++ config.py
@@ -30,3 +30,6 @@ OPENID_PROVIDERS = [
 ]
 
 POSTS_PER_PAGE = 3
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+MAX_SEARCH_RESULTS = 50
